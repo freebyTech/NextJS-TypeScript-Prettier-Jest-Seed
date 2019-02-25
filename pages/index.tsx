d228bafcb162ff878886ba985926498d67ecf681
James Eby <temporafugiunt@gmail.com>
2019-02-25 12:51:02 -0600

    styled-jsx examples for jsx styling.

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,36 +2,66 @@ import React from "react";
 import { Layout } from "../shared/Layout";
 import Link from "next/link";
 
-interface Props {
+interface IPost {
   id: string;
   title: string;
 }
 
-const PostLink: React.FunctionComponent<Props> = (props) => (
+const PostLink: React.FunctionComponent<IPost> = (props) => (
   <li>
     <Link as={`/post/${props.id}`} href={`/post?title=${props.title}`}>
       <a>{props.title}</a>
     </Link>
+    <style jsx>{`
+      li {
+        list-style: none;
+        margin: 5px 0;
+      }
+
+      a {
+        text-decoration: none;
+        color: blue;
+        font-family: "Arial";
+      }
+
+      a:hover {
+        opacity: 0.6;
+      }
+    `}</style>
   </li>
 );
 
-const PostFuncCompLink: React.FunctionComponent<Props> = (props) => (
-  <li>
-    <Link as={`/post/${props.id}`} href={`/postfunc?title=${props.title}`}>
-      <a>{props.title}</a>
-    </Link>
-  </li>
-);
+function getPosts(): IPost[] {
+  return [
+    { id: "hello-nextjs", title: "Hello Next.js" },
+    { id: "learn-nextjs", title: "Learn Next.js is awesome" },
+    { id: "deploy-nextjs", title: "Deploy apps with ZEIT" },
+  ];
+}
 
 const Index: React.FunctionComponent = () => {
   return (
     <Layout>
       <h1>My Blog</h1>
       <ul>
-        <PostLink id="hello-jextjs" title="Hello Next.js" />
-        <PostLink id="learn-nextjs" title="Learn Next.js is awesome" />
-        <PostFuncCompLink id="deploy-nextjs" title="Deploy apps with Zeit" />
+        {getPosts().map((post) => (
+          <PostLink key={post.id} id={post.id} title={post.title} />
+        ))}
       </ul>
+      <style jsx>{`
+        h1 {
+          font-family: "Arial";
+        }
+
+        ul {
+          padding: 0;
+        }
+
+        li {
+          list-style: none;
+          margin: 5px 0;
+        }
+      `}</style>
     </Layout>
   );
 };
--- pages/post.tsx
+++ pages/post.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { withRouter, SingletonRouter } from "next/router";
 import { Layout } from "../shared/Layout";
+import Markdown from "react-markdown";
 
 interface Props extends Partial<SingletonRouter> {
   // your props interface
@@ -12,7 +13,42 @@ export default class PostLink extends React.Component<Props> {
     return (
       <Layout>
         <h1>{this.props.router && this.props.router.query ? this.props.router.query.title : null}</h1>
-        <p>This is the blog post content.</p>
+        <div className="markdown">
+          <Markdown
+            source={`
+This is our blog post.
+Yes. We can have a [link](/link).
+And we can have a title as well.
+
+### This is a title
+
+And here's the content.
+     `}
+          />
+        </div>
+        <style jsx={true} global={true}>{`
+          h1 {
+            font-family: "Arial";
+          }
+          .markdown {
+            font-family: "Arial";
+          }
+
+          .markdown a {
+            text-decoration: none;
+            color: blue;
+          }
+
+          .markdown a:hover {
+            opacity: 0.6;
+          }
+
+          .markdown h3 {
+            margin: 0;
+            padding: 0;
+            text-transform: uppercase;
+          }
+        `}</style>
       </Layout>
     );
   }
