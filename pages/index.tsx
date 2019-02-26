2b8f64f9cc10cbe631b136f75668c254e0cb6ccd
James Eby <temporafugiunt@gmail.com>
2019-02-26 01:08:39 -0600

    Added some more material-ui buttons.

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,6 +17,12 @@ const styles = (theme) => ({
     textAlign: "center",
     paddingTop: theme.spacing.unit * 20,
   },
+  button: {
+    margin: theme.spacing.unit,
+  },
+  input: {
+    display: "none",
+  },
 });
 
 class Index extends React.Component {
@@ -68,6 +74,29 @@ class Index extends React.Component {
           <Button variant="contained" color="secondary" onClick={this.handleClick}>
             Super Secret Password
           </Button>
+          <div>
+            <Button variant="contained" className={classes.button}>
+              Default
+            </Button>
+            <Button variant="contained" color="primary" className={classes.button}>
+              Primary
+            </Button>
+            <Button variant="contained" color="secondary" className={classes.button}>
+              Secondary
+            </Button>
+            <Button variant="contained" color="secondary" disabled className={classes.button}>
+              Disabled
+            </Button>
+            <Button variant="contained" href="#contained-buttons" className={classes.button}>
+              Link
+            </Button>
+            <input accept="image/*" className={classes.input} id="contained-button-file" multiple type="file" />
+            <label htmlFor="contained-button-file">
+              <Button variant="contained" component="span" className={classes.button}>
+                Upload
+              </Button>
+            </label>
+          </div>
         </div>
       </Dashboard>
     );
